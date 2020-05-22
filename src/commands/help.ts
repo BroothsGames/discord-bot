99a6184e30e9985909ad60e33d9312a5351cae2e
gepetow <fadavidmar+1@gmail.com>
2020-05-22 10:14:53 -0500

    Update

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -5,28 +5,23 @@ module.exports.execute = async (message: Message, args: string, index: number) =
     let description: String = "";
 
     if (message.member?.hasPermission("ADMINISTRATOR")) {
-    for (let cycle = 0; cycle < commands_info.length ; cycle++) {
-        description = `${description} **Name**: ${commands_info[cycle].name}\n **Usage**: ${commands_info[cycle].usage}\n**Description**: ${commands_info[cycle].description}\n\n`
+        for (let cycle = 0; cycle < commands_info.length ; cycle++) {
+            description = `${description} **Name**: ${commands_info[cycle].name}\n **Usage**: ${commands_info[cycle].usage}\n**Description**: ${commands_info[cycle].description}\n\n`
+        }
     }
-
-    const embed = new MessageEmbed()
-    embed.setColor(`${color}`)
-    embed.setAuthor("Commands")
-    embed.setDescription(description)
-    message.author.send(embed)
-    } else if (!message.member?.hasPermission("ADMINISTRATOR")) {
+    else {
         for (let cycle = 0; cycle < commands_info.length ; cycle++) {
             if(commands_info[cycle].admin == false) {
                 description = `${description} **Name**: ${commands_info[cycle].name}\n **Usage**: ${commands_info[cycle].usage}\n**Description**: ${commands_info[cycle].description}\n\n`
             }
         }
-
-        const embed = new MessageEmbed()
-        embed.setColor(`${color}`)
-        embed.setAuthor("Commands")
-        embed.setDescription("");
-        embed.setDescription(description)
-        embed.setFooter("You are admin!")
-        message.author.send(embed)
     }
+    const embed = new MessageEmbed()
+    embed.setColor(`${color}`)
+    embed.setAuthor("Commands")
+    embed.setDescription(description)
+    if (message.member?.hasPermission("ADMINISTRATOR")) {
+        embed.setFooter("You are admin!")
+    }
+    message.author.send(embed)
 }
